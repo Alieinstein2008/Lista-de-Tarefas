2bd348a8bd349afb71e576916a2793c1f0bfd6f7
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-20 23:36:19 -0300

    :lipstick:	feat: Add animacoes e uma sombra abaixo do icone correspondente ao pathname na nav

--- lista-de-tarefas-nextjs/src/app/ui/nav-links.tsx
+++ lista-de-tarefas-nextjs/src/app/ui/nav-links.tsx
@@ -7,11 +7,11 @@ import {
 } from '@heroicons/react/24/outline';
 
 import Link from 'next/link';
-import { usePathname} from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
 const links = [
-  {name:'Home', href:'/home', icon:HomeIcon},
+  { name: 'Home', href: '/home', icon: HomeIcon },
   { name: 'Calendar', href: '/home/calendar', icon: CalendarIcon },
   { name: 'User', href: '/home/user', icon: UserIcon },
   { name: 'Config', href: '/home/configuration', icon: Cog6ToothIcon },
@@ -25,33 +25,41 @@ export default function NavLinks() {
       {
         links.map((link) => {
           const LinkIcon = link.icon;
-          return(
+          return (
             <Link
               href={link.href}
               key={link.name}
-              className={clsx(
-                'flex flex-col w-full md:justify-center md:p-3.5  hover:bg-gray-50 gap-1.5 items-center',
-                {'bg-gray-100 text-black ': pathname === link.href,},
-
-              )}
+              className={
+                clsx(
+                  'flex flex-col w-full md:justify-center md:p-3.5 gap-1.5 items-center',
+                  { ' text-black ': pathname === link.href },
+                )}
             >
-              
-              <p 
-              title={link.name} 
-              aria-label={link.name}
-              className='md:w-8 md:h-8 w-8 h-8 '
+
+              <nav
+                title={link.name}
+                aria-label={link.name}
+                className='md:w-10 md:h-10 w-10 h-10 flex flex-col justify-center align-middle items-center '
               >
-                <LinkIcon></LinkIcon>
-                
-              </p>
-              <div className={clsx(
-                '',
-                {'w-3 h-3 bg-blue-600 rounded-full border-cyan-500 border-1  ': pathname === link.href}
-              )}></div>
+                <LinkIcon className={
+                  clsx(
+                    'hover:-translate-y-0.5',
+                    { 'transition delay-500 linear -translate-y-2.5': pathname === link.href }
+                  )
+                }></LinkIcon>
+
+                <div className={
+                  clsx(
+                    'w-1 h-0.5 ',
+                    { 'transition-transform delay-700 h-0.5 duration-1000 linear bg-gray-700 -translate-y-2 mt-0.5 scale-x-450': pathname == link.href }
+                  )}></div>
+
+              </nav>
             </Link>
           )
 
         })
       }
     </>
-  )};
+  )
+};
