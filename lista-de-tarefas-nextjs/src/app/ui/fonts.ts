2c726483da4df6dd57959536b360e699d604eb5e
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-28 16:55:54 -0300

    :lipstick: feat: Adicionando Poppins font como var para usos posteriores

--- lista-de-tarefas-nextjs/src/app/ui/fonts.ts
+++ lista-de-tarefas-nextjs/src/app/ui/fonts.ts
@@ -1,5 +1,13 @@
-import {Roboto_Condensed} from 'next/font/google';
+import { Poppins, Roboto_Condensed } from 'next/font/google';
+
 export const roboto_condensed = Roboto_Condensed({
-    subsets:['latin'],
-    weight:['400']
-})
+    subsets: ['latin'],
+    weight: ['400']
+});
+
+export const poppins = Poppins({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-poppins',
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
