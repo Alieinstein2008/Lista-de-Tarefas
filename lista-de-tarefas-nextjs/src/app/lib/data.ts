bde1d9e175d3140793549a17b53e134eb9c3cb54
Arthur Alonso <arthuralonso8356@gmail.com>
2026-04-01 20:25:20 -0300

    :recycle: refactor: Alterando dados retirados do banco de dados

--- lista-de-tarefas-nextjs/src/app/lib/data.ts
+++ lista-de-tarefas-nextjs/src/app/lib/data.ts
@@ -1,61 +1,45 @@
 import postgres from 'postgres';
-import { Task, Label } from './definitions';
-
+import { BoardData } from './definitions';
+import { DEFAULT_BOARD_LABEL_DATA } from './constants';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-export async function fetchLabelsByUser({ userName, sessionUserId }: { userName: string, sessionUserId:string}) {
-    try {
-        const data = await sql<Label[]>`
-            SELECT
-                labels.id, labels.title,labels.user_id
-            FROM labels
-            LEFT JOIN tasks ON tasks.label_id = labels.id
-            WHERE labels.user_id = ${sessionUserId} 
-        
-        `;
-        console.log(data);
-        return data;
-    } catch (error) {
-        console.log(error);
-        return [];
+const users = {
+    name: 'Alonso',
+    email: 'alonso@gmail.com',
+    password: '12345678'
 
-
-    }
-};
-
-export async function seedDefaultsLabels() {
-    try {
-
-    } catch (error) {
-
-    }
 }
 
 
+export async function fetchBoardDataByUser({ userName, sessionUserId }: { userName: string, sessionUserId: string }) {
 
-export async function fetchTasks() {
     try {
-        const data = await sql<Task[]>`
-        SELECT * FROM tasks 
+        const data = await sql<BoardData[]>`
+             select 
+                "Label".public_id as label_public_id, 
+                "Label".title as label_title, 
+                "Priority".public_id as priority_public_id, 
+                "Priority".tag as priority_tag, 
+                "Priority".color as priority_color,
+                "Task".public_id as task_public_id, 
+                "Task".description as task_description, 
+                "Task".starts_in, 
+                "Task".expires_in
+                
+            from "Label" 
+
+            left join "Task" on "Label".id = "Task".label_id
+            left join "Priority" on "Priority".id = "Task".priority_id
+            
+            where "Label".user_id = ${sessionUserId}
+
         `;
-        console.log(data)
-    } catch (error) {
-        console.log(error)
-    }
-};
+        return (data);
 
-export async function fetchTasksByUser({ userName }: { userName: string; }) {
-    try {
-        const data = await sql<Task[]>`
-            SELECT  users.name, tasks.id, tasks.description, tasks.starts_in, tasks.expires_in, priorities.tag, priorities.color
-            FROM tasks
-            INNER JOIN users on tasks.user_id = users.id AND users.name LIKE ${userName}
-            INNER JOIN priorities ON tasks.priority_id = priorities.id
-         `;
-
-        return (data)
     } catch (error) {
-        console.log(error)
+
+        return DEFAULT_BOARD_LABEL_DATA;
+        
     }
 }
