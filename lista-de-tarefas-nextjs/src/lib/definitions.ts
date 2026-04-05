694d6dd8eebedb99b05e2d00df3f4c9780ffa7fa
Arthur Alonso <arthuralonso8356@gmail.com>
2026-04-05 18:25:23 -0300

    :broom: cleanup: Modificando caminho do diretorio lib

--- lista-de-tarefas-nextjs/src/lib/definitions.ts
+++ lista-de-tarefas-nextjs/src/lib/definitions.ts
@@ -21,6 +21,19 @@ export type Label = {
     quantity: number;
     tasks:[];
 }
+
+export type BoardData = {
+    label_public_id: string;
+    label_title: string;
+    priority_public_id: string;
+    priority_tag: string;
+    priority_color: string;
+    task_public_id: string;
+    task_description: string;
+    task_starts_in: string;
+    task_expires_in: string;
+}
+
 export const defaultsLabels = [
     { name: 'Nova Tarefa', quantity: 4 },
     { name: 'Agendada', quantity: 4 },
