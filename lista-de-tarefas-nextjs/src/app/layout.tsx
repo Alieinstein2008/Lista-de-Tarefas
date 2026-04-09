fa1ea1179c659213af0d94d000c2ead0146d7df9
Arthur Alonso <arthuralonso8356@gmail.com>
2026-04-08 22:19:08 -0300

    :lipstick: feat: Adicionando componente sidebar que deve aparecer durante todo o uso do usuario no sistema

--- lista-de-tarefas-nextjs/src/app/layout.tsx
+++ lista-de-tarefas-nextjs/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SideNav from "./ui/sidenav";
+import SideNav from "./ui/Sidenav";
 
 
 const geistSans = Geist({
