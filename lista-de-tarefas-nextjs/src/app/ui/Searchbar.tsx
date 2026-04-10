c906683b1345ca9e686448f4663aac00feb11fc2
Arthur Alonso <arthuralonso8356@gmail.com>
2026-04-09 22:08:38 -0300

    :lipstick: feat: Adicionando visual componente ui Searchbar

---
+++ lista-de-tarefas-nextjs/src/app/ui/Searchbar.tsx
@@ -0,0 +1,19 @@
+import { Search } from "lucide-react";
+import { poppins } from "./fonts";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { ButtonGroup } from "@/components/ui/button-group";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+export default function Searchbar() {
+    return (
+       <Field className={`${poppins.className} w-1/5 justify-self-end mt-2.5  `}>
+            <ButtonGroup>
+                <Input id="search-group" placeholder="Pesquisar Tarefas"></Input>
+                <Button variant={'outline'}>
+                    <Search></Search>
+                </Button>
+            </ButtonGroup>
+       </Field>
+
+    )
+}
